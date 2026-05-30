6a5e74f2a5213bd15596723d007463fbfac72c83
Etasura <etasura1968@gmail.com>
2026-05-30 09:03:42 +0900

    add minimal terraform s3 resource

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "portfolio_test" {
+  bucket = "etasura-terraform-test-20260530-01"
+
+  tags = {
+    Name        = "terraform-test-bucket"
+    Environment = "practice"
+    ManagedBy   = "terraform"
+  }
+}
